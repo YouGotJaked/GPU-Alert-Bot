0f25a04689594fab8243c5082d6f78175ed4ca6c
Jake Day <jaked97@gmail.com>
2018-03-19 15:36:34 -0700

    Implement logging of target hits

--- gpu_alert.py
+++ gpu_alert.py
@@ -1,9 +1,10 @@
 import praw
 import config
 import time
-import os.path
+import os
+import sys
 
-my_file = "gpu.txt"
+my_file = "log.txt"
 
 # log into reddit using config.py
 def login():
@@ -26,26 +27,41 @@ def run_bot(reddit,target,sub,lim,secs):
     for submission in subreddit.new(limit = lim):
         title = submission.title
         if target in title:
+            print "\nTarget found in '%s'" % title
             log_url(my_file,submission.url)
             # email url to self
 
-    print "Sleeping for",secs,"seconds..."
+    print "\nSleeping for",secs,"seconds..."
     time.sleep(secs)
 
 # log the urls in a text file
 def log_url(file,url):
+    #check if file exists
+    if not os.path.isfile(my_file):
+        print "\nFile %s does not exist. Creating file..." % my_file
+        f = open(file,"a+")
+        f.close()
+    
     # open file for reading and appending
-    with open(file,"a+") as f:
-            print "\nAdded %s to file on %s" % (url,current_time())
+    with open(file,"r+") as f:
+        if os.path.getsize(file) == 0:
+            print "\n%s: File is empty. Adding '%s' to file." % (current_time(),url)
             f.write("%s\n\n" % url)
+        else:
+            if url in f.read():
+                print "URL already in file!"
+            else:
+                print "\n%s: Added '%s' to file." % (current_time(),url)
+                f.write("%s\n\n" % url)
 
 def current_time():
     return time.asctime(time.localtime(time.time()))
 
 def main():
+    #TODO: get keyword and subreddit from user input
     reddit = login()
     while True:
-        run_bot(reddit,"1080",'buildapcsales',25,2)
+        run_bot(reddit,"1080",'buildapcsales',50,2)
 
 if __name__== "__main__":
     main()
