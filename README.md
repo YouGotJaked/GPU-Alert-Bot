bc031c4e6bfe895408fa6387568d94ddc15ee16c
Jake Day <jaked97@gmail.com>
2018-03-23 13:17:54 -0700

    Add email notification

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # GPU-Alert-Bot
-Python bot for Reddit that continuously checks /r/buildapcsales for posts containing a specified keyword. Written in Python 2.7.10.
+Python bot for Reddit that continuously checks user-provided subreddit such as /r/buildapcsales or /r/hardwareswap for posts containing a specified keyword. Written in Python 2.7.10.
--- gpu_alert.py
+++ gpu_alert.py
@@ -3,7 +3,10 @@ import config
 import time
 import os
 import sys
+import smtplib
+from email.mime.text import MIMEText
 
+addr = "jaked97@gmail.com"
 my_file = "log.txt"
 count = 0
 secs = 2
@@ -30,9 +33,8 @@ def run_bot(reddit,target,sub,lim):
         title = submission.title
         if target.lower() in title.lower():
             print "\nTarget found in '%s'" % title
-            log_url(my_file,submission.url)
-            # email url to self
-
+            if log_url(my_file,submission.url):
+                send_email(title,submission.url)
     sleep()
 
 # log the urls in a text file
@@ -54,9 +56,20 @@ def log_url(file,url):
             if url in f.read():
                 print "URL already in file!"
                 count = count + 1
+                return False
             else:
                 print "\n%s: Added '%s' to file." % (current_time(),url)
                 f.write("%s\n\n" % url)
+        return True
+
+def send_email(title,url):
+    msg = MIMEText(url)
+    msg['Subject'] = title
+    msg['From'] = addr
+    msg['To'] = addr
+    s = smtplib.SMTP('localhost', 1025)
+    s.sendmail(addr, [addr], msg.as_string())
+    s.quit()
 
 def current_time():
     return time.asctime(time.localtime(time.time()))
@@ -64,12 +77,12 @@ def current_time():
 def sleep():
     global count,secs
     
-    # TODO: change 50 to constant threshold
-    if count > 50:
+    # TODO: change 25 to constant threshold
+    if count > 25:
         print "\nThreshold hit."
         count = 0
         secs = 60
-    else:
+    elif secs != 2:
         secs = 2
 
     print "\nSleeping for %d seconds... count is %d" % (secs,count)
@@ -78,20 +91,20 @@ def sleep():
 def user_input():
     try:
         keyword = raw_input("\nEnter a keyword to search: ")
-        return keyword
+        subreddit = raw_input("\nEnter a subreddit to search: ")
+        return keyword,subreddit
     except (EOFError,KeyboardInterrupt):
         print "\nExiting %s" % sys.argv[0]
         sys.exit(0)
 
 def main():
     reddit = login()
-
-    keyword = user_input()
+    keyword,subreddit = user_input()
 
     while True:
         print "\nPress CTRL-C to exit program at any time."
         try:
-            run_bot(reddit,keyword,'buildapcsales',50)
+            run_bot(reddit,keyword,subreddit,150)
         except (EOFError,KeyboardInterrupt):
             print "\nExiting %s" % sys.argv[0]
             sys.exit(0)
